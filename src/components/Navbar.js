79ca0a7a9f1f1331e8af70c2d02fa076929c2d4f
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-03-04 10:23:22 +0300

    initialize empty project and install tailwind

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <>
+      <h2>navbar here</h2>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>IMDb clone | about</title>
+        <meta name='description' content='IMDb clone' />
+      </Head>
+      <h1>About</h1>
+    </>
+  )
+}
+
+export default About
