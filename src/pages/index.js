f253d53087b05d9ae49ec237cd63d87efdec2016
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-03-05 13:30:53 +0300

    handle errors with a 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,35 @@ import { useEffect, useState } from 'react'
 
 const API_KEY = process.env.API_KEY
 
-export default function Home() {
+export default function Home({ results }) {
+  console.log(results)
   const router = useRouter()
 
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(results)
+  console.log(data)
 
   useEffect(() => {
-    const { genre } = router.query || { genre: 'fetchTrending' }
-    const fetchData = async () => {
-      const res = await fetch(
-        `https://api.themoviedb.org/3/${
-          genre === 'fetchTrending' ? 'trending/all/week' : 'movie/top_rated'
-        }?api_key=560696d03cbc7f14fb10307bf0faded6&language=en-US&page=1`
-      )
-      const data = await res.json()
-      setData(data.results)
+    const { genre } = router.query
+    console.log('here is movie genre', genre)
+    if (genre) {
+      const fetchData = async () => {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/${
+            genre === 'fetchTrending' ? 'trending/all/week' : 'movie/top_rated'
+          }?api_key=560696d03cbc7f14fb10307bf0faded6&language=en-US&page=1`
+        )
+        if (!res.ok) {
+          throw new Error('Something went wrong')
+        }
+        const data = await res.json()
+
+        setData(data.results)
+      }
+      fetchData()
+    } else if (!genre) {
+      setData(results)
     }
-    fetchData()
-  }, [router.query])
+  }, [router.query, results])
   // const { genre } = router.query
   console.log(data)
 
@@ -43,3 +54,15 @@ export default function Home() {
 }
 
 // using getStaticProps and getStaticPaths to fetch data
+
+export async function getStaticProps() {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/popular/?api_key=560696d03cbc7f14fb10307bf0faded6&language=en-US&page=1`
+  )
+  const data = await res.json()
+  return {
+    props: {
+      results: data.results,
+    },
+  }
+}
